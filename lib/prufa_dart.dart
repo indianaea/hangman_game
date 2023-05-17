98b751c27b23f900637971971e94745e2dd65c14
indianaea <126889057+indianaea@users.noreply.github.com>
2023-05-17 21:49:53 +0000

    breytingar í dart, pælingar með skiptingu fyrir að setja upp með flutter

--- lib/prufa_dart.dart
+++ lib/prufa_dart.dart
@@ -2,11 +2,15 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  final List<String> words = ['epli', 'banani', 'sól', 'bíll', 'ullarsokkur'];
+  playGame();
+}
+
+void playGame() {
+  final List<String> words = ['Epli', 'banani', 'sól', 'bíll', 'ullarsokkur'];
   final Random random = Random();
   final String selectedWord = words[random.nextInt(words.length)];
   List<String> guessedLetters = [];
-  int attemptsLeft = 6;
+  int attemptsLeft = 5;
 
   print('Velkomin í Hengimann!');
   print('Lengd orðsins er: ${selectedWord.length} stafir.');
@@ -14,53 +18,50 @@ void main() {
   while (attemptsLeft > 0) {
     print('Líf eftir: $attemptsLeft');
     print('Orðið: ${getVisibleWord(selectedWord, guessedLetters)}');
-    print('Giskaðu á það:');
+    print('Giskaðu á það: ');
     final String guess = stdin.readLineSync()?.toLowerCase() ?? '';
 
     if (guess.length != 1) {
-      print('Settu inn einn staf.');
-      continue;
-    }
-
-    if (guessedLetters.contains(guess)) {
+      print('Vinsamlegast settu inn einn staf.');
+    } else if (guessedLetters.contains(guess)) {
       print('Þú ert nú þegar búin að giska á þennan staf.');
-      continue;
-    }
-
-    guessedLetters.add(guess);
-
-    if (selectedWord.contains(guess)) {
-      print('Rétt gisk!');
-      if (checkWin(selectedWord, guessedLetters)) {
-        print('Til hamingju þú vannst!');
-        break;
-      }
     } else {
-      print('Rangt gisk!');
-      attemptsLeft--;
-      if (attemptsLeft == 0) {
-        print('Leik lokið! Þú tapaðir því miður. Orðið var: $selectedWord');
+      guessedLetters.add(guess);
+
+      if (selectedWord.toLowerCase().contains(guess)) {
+        print('Rétt gisk');
+        if (checkWin(selectedWord, guessedLetters)) {
+          print('Til hamingju þú vannst!');
+          break;
+        }
+      } else {
+        print('Rangt gisk!');
+        attemptsLeft--;
+        if (attemptsLeft == 0) {
+          print('Leik lokið! Þú tapaðir því miður. Orðið var: $selectedWord');
+        }
       }
     }
+  }
+
+  print('Viltu spila aftur? (Já/Nei)');
+  final String playAgain = stdin.readLineSync()?.toLowerCase() ?? '';
+  if (playAgain == 'já') {
+    playGame();
   }
 }
 
 String getVisibleWord(String word, List<String> guessedLetters) {
-  String visibleWord = '';
-  for (int i = 0; i < word.length; i++) {
-    if (guessedLetters.contains(word[i])) {
-      visibleWord += word[i];
-    } else {
-      visibleWord += '_';
-    }
-    visibleWord += ' ';
-  }
-  return visibleWord.trim();
+  List<String> visibleLetters = word.split('').map((letter) {
+    return guessedLetters.contains(letter.toLowerCase()) ? letter : '_';
+  }).toList();
+
+  return visibleLetters.join(' ');
 }
 
 bool checkWin(String word, List<String> guessedLetters) {
   for (int i = 0; i < word.length; i++) {
-    if (!guessedLetters.contains(word[i])) {
+    if (!guessedLetters.contains(word[i].toLowerCase())) {
       return false;
     }
   }
