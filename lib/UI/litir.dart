d30bfea3cf76a8e5c799473b1b6a2454366638ae
indianaea <126889057+indianaea@users.noreply.github.com>
2023-05-18 15:54:37 +0000

    setja upp og velja lit

---
+++ lib/UI/litir.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class appLitir{
+  static Color primaryColor = Color(0XFF80DEEA);
+  static Color primaryColorDark = Color(0XFF80DEEA);
+}
--- lib/prufa_dart.dart
+++ lib/prufa_dart.dart
@@ -16,20 +16,15 @@ void playGame() {
   print('Lengd orðsins er: ${selectedWord.length} stafir.');
 
   while (attemptsLeft > 0) {
-    print('Líf eftir: $attemptsLeft');
-    print('Orðið: ${getVisibleWord(selectedWord, guessedLetters)}');
-    print('Giskaðu á það: ');
-    final String guess = stdin.readLineSync()?.toLowerCase() ?? '';
+    printStatus(attemptsLeft, selectedWord, guessedLetters);
+    final String guess = readGuess();
 
-    if (guess.length != 1) {
-      print('Vinsamlegast settu inn einn staf.');
-    } else if (guessedLetters.contains(guess)) {
-      print('Þú ert nú þegar búin að giska á þennan staf.');
-    } else {
+    if (isValidGuess(guess, guessedLetters)) {
       guessedLetters.add(guess);
 
-      if (selectedWord.toLowerCase().contains(guess)) {
+      if (isCorrectGuess(guess, selectedWord)) {
         print('Rétt gisk');
+
         if (checkWin(selectedWord, guessedLetters)) {
           print('Til hamingju þú vannst!');
           break;
@@ -37,6 +32,7 @@ void playGame() {
       } else {
         print('Rangt gisk!');
         attemptsLeft--;
+
         if (attemptsLeft == 0) {
           print('Leik lokið! Þú tapaðir því miður. Orðið var: $selectedWord');
         }
@@ -44,8 +40,39 @@ void playGame() {
     }
   }
 
+  playAgain();
+}
+
+void printStatus(int attemptsLeft, String selectedWord, List<String> guessedLetters) {
+  print('Líf eftir: $attemptsLeft');
+  print('Orðið: ${getVisibleWord(selectedWord, guessedLetters)}');
+}
+
+String readGuess() {
+  print('Giskaðu á staf: ');
+  final String guess = stdin.readLineSync()?.toLowerCase() ?? '';
+  return guess;
+}
+
+bool isValidGuess(String guess, List<String> guessedLetters) {
+  if (guess.length != 1) {
+    print('Vinsamlegast settu inn einn staf.');
+    return false;
+  } else if (guessedLetters.contains(guess)) {
+    print('Þú ert nú þegar búin að giska á þennan staf.');
+    return false;
+  }
+  return true;
+}
+
+bool isCorrectGuess(String guess, String selectedWord) {
+  return selectedWord.toLowerCase().contains(guess);
+}
+
+void playAgain() {
   print('Viltu spila aftur? (Já/Nei)');
   final String playAgain = stdin.readLineSync()?.toLowerCase() ?? '';
+
   if (playAgain == 'já') {
     playGame();
   }
