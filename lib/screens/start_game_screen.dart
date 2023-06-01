99fc0c0e65d8848b470335493a38c3456b0ac86d
indianaea <126889057+indianaea@users.noreply.github.com>
2023-06-01 22:25:30 +0000

    bæta við widgetinu Flexible á fyrsta skjáinn svo contentið virki á sem flestum skjástærðum

--- lib/screens/start_game_screen.dart
+++ lib/screens/start_game_screen.dart
@@ -22,60 +22,66 @@ class StartScreen extends StatelessWidget {
     return ScreenContainer(
       title: 'Hengimaður',
       children: [
-        Transform.translate(
-          offset: Offset(0, -30.0), // Adjust the y-offset as needed
-          child: Image.asset(
-            'assets/front.png',
-            width: 350, // Adjust width as needed
-            height: 350, // Adjust height as needed
+        Flexible(
+          flex: 3,
+          child: Transform.translate(
+            offset: Offset(0, -30.0),
+            child: Image.asset(
+              'assets/front.png',
+            ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 30),
-          child: Center(
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'Aðeins um leikinn\n',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 19.0,
-                      fontWeight: FontWeight.bold,
+        Flexible(
+          flex: 2,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 25),
+            child: Center(
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Aðeins um leikinn\n',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  TextSpan(
-                    text:
-                        '\nLeikurinn snýst um að spilari fær random orð og \nhann reynir svo að giska á það orð áður en full \nmynd af Hengimanni er teiknuð upp. Ef spilari nær \nekki að giska á rétt orð fær hann Hengimann. \n',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.normal,
+                    TextSpan(
+                      text:
+                          '\nLeikurinn snýst um að spilari fær random orð og \nhann reynir svo að giska á það orð áður en full \nmynd af Hengimanni er teiknuð upp. Ef spilari nær \nekki að giska á rétt orð fær hann Hengimann. \n',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.normal,
+                      ),
                     ),
-                  ),
-                  TextSpan(
-                    text:
-                        '\nÞú hefur 9 líf \nÞú færð eina vísbendingu gefna eftir 2 tilraunir',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
+                    TextSpan(
+                      text:
+                          '\nÞú hefur 9 líf \nÞú færð eina vísbendingu gefna eftir 2 tilraunir',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
-        SizedBox(height: 20.0),
-        Transform.translate(
-          offset: Offset(0, -10), // change value as needed
-          child: CustomButton(
-            text: 'Spila leik',
-            onPressed: () => _selectPlayGame(context),
-            buttonWidth: 140,
-            buttonHeight: 40,
+        Flexible(
+          flex: 1,
+          child: Transform.translate(
+            offset: Offset(0, -10),
+            child: CustomButton(
+              text: 'Spila leik',
+              onPressed: () => _selectPlayGame(context),
+              buttonWidth: 140,
+              buttonHeight: 40,
+            ),
           ),
         ),
       ],
