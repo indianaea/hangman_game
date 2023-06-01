bdba1a9cb7400bbfc7d279ec776aa2ac30b0a33b
indianaea <126889057+indianaea@users.noreply.github.com>
2023-06-01 01:08:27 +0000

    minor breyting

--- lib/widgets/hangman_photo.dart
+++ lib/widgets/hangman_photo.dart
@@ -3,13 +3,15 @@ import 'package:flutter/cupertino.dart';
 // Fall sem býr til widget fyrir Hengimann myndina.
 // Fallið tekur inn breyturnar streng og boolean til að
 // ákvarða hvort sýna eigi ákveðna mynd af Hengimanninum.
-Widget hangmanPhoto(bool visible, String path){
+Widget hangmanPhoto(bool visible, String path) {
   return Visibility(
       visible: visible,
-      child: Container(
-        width: 270,
-        height: 270,
-        child: Image.asset(path),
-      )
-  );
+      child: Padding(
+        padding: EdgeInsets.only(top: 15), // Add space above the picture
+        child: Container(
+          width: 270,
+          height: 270,
+          child: Image.asset(path),
+        ),
+      ));
 }
